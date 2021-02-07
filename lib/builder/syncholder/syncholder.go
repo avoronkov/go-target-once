46cb5215107acc9d3600837e2a2b515cda54933c
Aleksandr Voronkov <avoronkov@plesk.com>
2021-02-07 14:40:57 +0700

    Implement syncholder using sync.Cond

--- lib/builder/syncholder/syncholder.go
+++ lib/builder/syncholder/syncholder.go
@@ -13,61 +13,53 @@ import (
 //line syncholder.go2:2
 )
 
-//line syncholder.go2:54
+//line syncholder.go2:46
 type ResultSyncHolder = instantiate୦୦SyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult
 
 func NewResultSyncHolder() *ResultSyncHolder {
 	return instantiate୦୦NewSyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult()
 }
 
-//line syncholder.go2:58
+//line syncholder.go2:50
 type instantiate୦୦SyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult struct {
 //line syncholder.go2:12
  data  *targets.Result
 				ready bool
-				mutex sync.Mutex
 
-				observers []chan *targets.Result
+				cond *sync.Cond
 }
 
-//line syncholder.go2:23
+//line syncholder.go2:24
 func (o *instantiate୦୦SyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult,) Put(data *targets.Result) {
-	o.mutex.Lock()
-	defer o.mutex.Unlock()
+	o.cond.L.Lock()
+	defer o.cond.L.Unlock()
 
 	o.data = data
 	o.ready = true
 
-	for _, observer := range o.observers {
-		observer <- data
-		close(observer)
-	}
-	o.observers = []chan *targets.Result{}
+	o.cond.Broadcast()
 }
 
 func (o *instantiate୦୦SyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult,) Get() *targets.Result {
-	o.mutex.Lock()
+	o.cond.L.Lock()
+	defer o.cond.L.Unlock()
 
-	if o.ready {
-		o.mutex.Unlock()
-		return o.data
+	for !o.ready {
+		o.cond.Wait()
 	}
 
-//line syncholder.go2:46
- ch := make(chan *targets.Result)
-	o.observers = append(o.observers, ch)
-	o.mutex.Unlock()
-
-	return <-ch
+	return o.data
 }
-//line syncholder.go2:19
+//line syncholder.go2:18
 func instantiate୦୦NewSyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult() *instantiate୦୦SyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult {
-	return &instantiate୦୦SyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult{}
+	return &instantiate୦୦SyncHolder୦୮1github୮acom୮davoronkov୮dgo୮ctarget୮conce୮dlib୮dtargets୮aResult{
+		cond: sync.NewCond(new(sync.Mutex)),
+	}
 }
 
-//line syncholder.go2:21
+//line syncholder.go2:22
 type Importable୦ int
-//line syncholder.go2:21
+//line syncholder.go2:22
 type _ targets.BuildContext
-//line syncholder.go2:21
+//line syncholder.go2:22
 type _ sync.Cond
