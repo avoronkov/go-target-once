71b1984c237fef8ef381c35b0a14a56f51757090
Aleksandr Voronkov <avoronkov@plesk.com>
2021-02-28 20:09:37 +0500

    Add methods to targets.URL

--- lib/targets/http.go
+++ lib/targets/http.go
@@ -9,7 +9,12 @@ import (
 )
 
 type Url struct {
-	url string
+	url        string
+	method     string
+	user, pass string
+	dynamic    bool
+	// TODO support multiple values
+	headers map[string]string
 }
 
 var _ Target = (*Url)(nil)
@@ -17,8 +22,26 @@ var _ Modifiable = (*Url)(nil)
 
 func NewUrl(url string) *Url {
 	return &Url{
-		url: url,
+		url:     url,
+		method:  http.MethodGet,
+		headers: make(map[string]string),
 	}
+}
+
+func (u *Url) SetBasicAuth(user, pass string) *Url {
+	u.user = user
+	u.pass = pass
+	return u
+}
+
+func (u *Url) Dynamic() *Url {
+	u.dynamic = true
+	return u
+}
+
+func (u *Url) SetHeader(key, value string) *Url {
+	u.headers[key] = value
+	return u
 }
 
 func (u *Url) TargetID() string {
@@ -26,7 +49,12 @@ func (u *Url) TargetID() string {
 }
 
 func (u *Url) Build(bc BuildContext) Result {
-	resp, err := http.Get(u.url)
+	req, err := http.NewRequest(u.method, u.url, nil)
+	if err != nil {
+		return Failed(err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return Failed(err)
 	}
@@ -47,6 +75,10 @@ func (u *Url) Build(bc BuildContext) Result {
 var lastModifiedFmt = time.RFC1123
 
 func (u *Url) IsModified(since time.Time) bool {
+	if u.dynamic {
+		return true
+	}
+
 	resp, err := http.Head(u.url)
 	if err != nil {
 		logger.Warningf("HEAD %v failed: %v", u.url, err)
